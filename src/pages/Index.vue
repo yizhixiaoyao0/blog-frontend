873bf13ed88905fc4be07940802a8066ec394b33
米娅莫拉苏娜丹妮谢莉红 <1251541611@qq.com>
2020-09-17 16:12:06 +0800

    first

---
+++ src/pages/Index.vue
@@ -0,0 +1,125 @@
+<template>
+  <div>
+    <Layout>
+    <!-- Page Header -->
+    <header class="masthead" 
+    :style="{backgroundImage: `url(${GRIDSOME_API_URL + general.cover.url})`}">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="site-heading">
+              <h1>{{general.title}}</h1>
+              <span class="subheading">{{general.subtitle}}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <!-- Main Content -->
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="post-preview" v-for='edge in $page.posts.edges' :key='edge.node.id'>
+            <g-link :to="'/post/' + edge.node.id">
+              <h2 class="post-title">
+               {{edge.node.title}}
+              </h2>
+              <!-- <h3 class="post-subtitle">
+                Problems look mighty small from 150 miles up
+              </h3> -->
+            </g-link>
+            <p class="post-meta">
+              Posted by
+              <a href="#">{{ edge.node.created_by.firstname + edge.node.created_by.lastname }}</a>
+              on {{ edge.node.created_at }}
+            </p>
+            <p>
+              <span v-for='tag in edge.node.tags' :key='tag.id'>
+                <g-link :to="'/tag/' + tag.id" > {{tag.title}}</g-link>
+              </span>
+            </p>
+            <hr />
+          </div>
+          <!-- Pager -->
+          <!-- <div class="clearfix">
+            <a class="btn btn-primary float-right" href="#"
+              >Older Posts &rarr;</a
+            >
+          </div> -->
+          <pager :info='$page.posts.pageInfo'/>
+        </div>
+      </div>
+    </div>
+
+    <hr />
+
+    </Layout>
+
+  </div>
+</template>
+
+<page-query>
+query($page: Int) {
+  posts: allStrapiPost (perPage: 1, page: $page) @paginate {
+    pageInfo {
+      totalPages
+      currentPage
+    }
+    edges {
+      node {
+        id
+        title
+        created_by {
+          id
+          firstname
+          lastname
+        }
+        tags {
+          id
+          title
+        }
+        created_at
+      }
+    }
+  }
+  general: allStrapiGeneral {
+    edges {
+      node {
+        id
+        title
+        subtitle
+        cover {
+          url
+        }
+      }
+
+    }
+  }
+}
+</page-query>
+
+<script>
+import {Pager} from 'gridsome'
+export default {
+  name: 'HomePage',
+  metaInfo: {
+    title: "Hello, world!",
+  },
+  components: {
+    Pager
+  },
+  computed: {
+    general() {
+      return this.$page.general.edges[0].node
+    }
+  }
+};
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
---
+++ src/templates/Tag.vue
@@ -0,0 +1,77 @@
+<template>
+   <Layout>
+    <!-- Page Header -->
+    <header class="masthead">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="site-heading">
+              <h1>#{{$page.tag.title}}</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <!-- Main Content -->
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="post-preview" v-for='post in $page.tag.posts' :key='post.id'>
+            <g-link :to="'/post/' + post.id">
+              <h2 class="post-title">
+               {{post.title}}
+              </h2>
+              <!-- <h3 class="post-subtitle">
+                Problems look mighty small from 150 miles up
+              </h3> -->
+            </g-link>
+            <!-- <p class="post-meta">
+              Posted by
+              <a href="#">{{ edge.node.created_by.firstname + edge.node.created_by.lastname }}</a>
+              on {{ edge.node.created_at }}
+            </p>
+            <p>
+              <span v-for='tag in edge.node.tags' :key='tag.id'>
+                <g-link :to="'/tag/' + tag.id" > {{tag.title}}</g-link>
+              </span>
+            </p> -->
+            <hr />
+          </div>
+          <!-- Pager -->
+          <!-- <div class="clearfix">
+            <a class="btn btn-primary float-right" href="#"
+              >Older Posts &rarr;</a
+            >
+          </div> -->
+          <!-- <pager :info='$page.posts.pageInfo'/> -->
+        </div>
+      </div>
+    </div>
+
+    <hr />
+
+    </Layout>
+</template>
+<page-query>
+query($id: ID!) {
+  tag: strapiTag(id: $id) {
+    id
+    title
+		posts {
+      id
+      title
+    }
+  }
+}
+</page-query>
+<script>
+export default {
+  name: 'TagPage'
+}
+</script>
+
+<style>
+
+</style>
